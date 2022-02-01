ae18fa1b9815451468b67bb7aa7ffec992e5ff96
Christopher <Chris1Bobsy@Gmail.com>
2022-02-01 18:59:32 +0100

    Update Header Banner

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <span className="header-title-large">Blog</span>
       </div>
       <img
-        src="https://images.pexels.com/photos/2775196/pexels-photo-2775196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt="Header"
         className="header-img"
       />
