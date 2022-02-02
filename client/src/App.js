31e982292b8bb8197ea7560b900350743bdb74ca
Christopher <Chris1Bobsy@Gmail.com>
2022-02-02 13:41:52 +0100

    Created Single Page Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
+import Single from "./Pages/Single/Single";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <Single />
     </div>
   );
 }
---
+++ client/src/Pages/Single/Single.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Post from "../../components/Post/Post";
+import Sidebar from "../../components/Sidebar/Sidebar";
+
+import "./Single.css";
+
+const Single = () => {
+  return (
+    <div>
+      {/* <Post /> */}
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Single;
