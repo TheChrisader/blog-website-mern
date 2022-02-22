615cef4b8eb81fd3d4c428d563b0cc1f45f4461a
Christopher <Chris1Bobsy@Gmail.com>
2022-02-22 23:20:48 +0100

    created "Create Page" component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar/Navbar";
+import Create from "./Pages/Create/Create";
 import Home from "./Pages/Home/Home";
 import Single from "./Pages/Single/Single";
 
@@ -7,7 +8,8 @@ function App() {
     <div>
       <Navbar />
       {/* <Home /> */}
-      <Single />
+      {/* <Single /> */}
+      <Create />
     </div>
   );
 }
