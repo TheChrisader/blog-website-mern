b9acce19ed5e048068bf74f089a41aa4dff49ff4
Christopher <Chris1Bobsy@Gmail.com>
2022-02-26 15:56:47 +0100

    Auth Route setup

--- api/models/User.js
+++ api/models/User.js
@@ -23,4 +23,4 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.Model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const authRoute = require("./routes/Auth");
+
 const app = express();
 dotenv.config();
 
@@ -17,6 +19,10 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Connected to server`);
